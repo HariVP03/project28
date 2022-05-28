0b7ff244358d93c5f21ba4137b618ca946e8cdae
HariVP03 <harryskotch11@gmail.com>
2022-05-28 09:39:38 +0530

    tested connection to mongodb

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,10 @@
 import express, { Response, Request } from "express";
 import http from "http";
 import path from "path";
+import { PrismaClient } from "@prisma/client";
 
 const port = process.env.PORT || 3000;
+const prisma = new PrismaClient();
 
 const app = express();
 const server = http.createServer(app);
@@ -11,10 +13,16 @@ app.use(
   express.static(path.join(__dirname, "/../frontend/.next/server/pages"))
 );
 
-app.get("*", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.sendFile(
     path.join(__dirname, "/../frontend/.next/server/pages", "index.html")
   );
+});
+
+app.get("/haha", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  // return users;
+  console.log({ users });
 });
 
 server.listen(port, () => {
